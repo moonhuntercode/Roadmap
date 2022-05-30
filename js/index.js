416b71079d1f8976c0a6fc5f362f23bea227a40c
victorcode <vdcloayza@gmail.com>
2022-05-30 00:22:46 -0400

    index html working with js successfully

--- js/index.js
+++ js/index.js
@@ -3,4 +3,10 @@ const loadInitialTemplate=()=>{
     `
     <h1>hola mundaso</h1>
     `
+    const body=document.getElementsByTagName('body')[0];
+    body.innerHTML=template;
+}
+
+window.onload=()=>{
+    loadInitialTemplate();
 }
