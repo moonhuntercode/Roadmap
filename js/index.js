77c8ed1f35867dcbde608ee77b9d8b55c96beea2
victorcode <vdcloayza@gmail.com>
2022-06-05 21:51:11 -0400

    view 1 added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 const loadInitialTemplate=()=>{
     const template= //html
     `
-    <h1>hola mundaso</h1>
+    <div class="container1">
+    <h1>REPASOS</h1>
+    <div className="navbar">
+    <nav>
+    <a href="/inicio">inicio</a>
+    <a href="/inicio">courses</a>
+    <a href="/inicio">contact</a>
+    </nav>
+    </div>
+    </div>
     `
     const body=document.getElementsByTagName('body')[0];
     body.innerHTML=template;
